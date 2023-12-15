a20ac57a5c67f739ba81f60fc19d9b5910b45b80
Roman Bochkarev <allhailglukko@gmail.com>
2023-12-15 13:52:30 +0500

    Change App.java and add plugin into build.gradle.kts

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,6 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to the Brain Games!");
     }
 }
